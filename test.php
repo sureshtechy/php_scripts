1b8791528481069f2399e95fae9b5504710819f9
sureshtechy <96573254+sureshtechy@users.noreply.github.com>
2021-12-23 15:43:06 +0530

    script file upload

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+  	echo "Hello World!";
+    $myfile = fopen("text.txt", "r") or die("Unable to open file!");
+    $text = fread($myfile,filesize("text.txt"));
+    preg_match_all("/\[[^\]]*\]/", $text, $matches);
+    print_r($matches);
+    var_dump($matches[0]);
+    fclose($myfile);
+?>
+
