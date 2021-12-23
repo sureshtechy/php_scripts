2f261b092a7c598e880f40a1f145cc207423a4f5
sureshtechy <96573254+sureshtechy@users.noreply.github.com>
2021-12-23 16:10:55 +0530

    deleting file

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $myfile = fopen("text.txt", "r") or die("Unable to open file!");
-    $text = fread($myfile,filesize("text.txt"));
-    preg_match_all("/\[[^\]]*\]/", $text, $matches);
-    print_r($matches);
-    var_dump($matches[0]);
-    fclose($myfile);
-?>
-
